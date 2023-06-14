86881697072e4bfd8b49d281171659a8191ea4c8
HackerSM9 <sm9hacking@gmail.com>
2023-06-14 15:38:58 +0530

    .

--- script.js
+++ script.js
@@ -43,6 +43,6 @@ if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
 
 // When the user clicks on the button, scroll to the top of the document
 function topFunction() {
-document.body.scrollTop = 1;
-document.documentElement.scrollTop = 1;
+document.body.scrollTop = 0;
+document.documentElement.scrollTop = 0;
 }
